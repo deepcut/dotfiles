087c49492aa90d46f165761815f24efb182da048
Fei <feifanw@gmail.com>
2019-12-08 23:14:14 -0500

    Add personal convenience aliases

--- zsh.after/aliases.zsh
+++ zsh.after/aliases.zsh
@@ -3,6 +3,14 @@ alias rezsh="source ~/.zshrc"
 alias dot="cd ~/.dotfiles"
 alias vi="nvim"
 
+# Grep processes
 alias pse='ps aux | grep elixir'
 alias psn='ps aux | grep node'
 
+# Elixir/Phoenix
+alias exiex="iex -S mix"
+alias exserve="iex -S phx.server"
+
+# Personal convenience
+alias smbe="cd ~/Documents/SM_Local/sm_umbrella/"
+alias smfe="cd ~/Documents/SM_Local/sm_shopify_app/"
