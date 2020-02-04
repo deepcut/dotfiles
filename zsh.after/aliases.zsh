65cd525a7dff7dbc2250ddd03884c400da6eccc0
Feifan Wang <feifanw@gmail.com>
2020-02-03 22:18:43 -0500

    add .env to env with alias

--- zsh.after/aliases.zsh
+++ zsh.after/aliases.zsh
@@ -12,8 +12,8 @@ alias pse='ps aux | grep elixir'
 alias psn='ps aux | grep node'
 
 # Elixir/Phoenix
-alias exiex="iex -S mix"
-alias exserve="iex -S mix phx.server"
+alias exiex="source .env; iex -S mix"
+alias exserve="source .env; iex -S mix phx.server"
 
 # Personal convenience
 alias smbe="cd ~/Documents/SM_Local/sm_umbrella/"
