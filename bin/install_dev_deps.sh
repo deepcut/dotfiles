f8bdae90100eaa8bfb68d67c0b1564e603e1f9d3
Feifan Wang <feifanw@gmail.com>
2020-02-03 22:19:20 -0500

    Add two yadr vim plugins for js and jsx

--- bin/install_dev_deps.sh
+++ bin/install_dev_deps.sh
@@ -14,7 +14,10 @@ asdf plugin-update --all
 # Tmux
 git clone "https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm"
 
+# Vim Utilities
+yadr-vim-add-plugin --url=https://github.com/pangloss/vim-javascript
+yadr-vim-add-plugin --url=https://github.com/MaxMEllon/vim-jsx-pretty
+
 # Utility CLIs
 echo "Shopify CLI"
 eval "$(curl -sS https://raw.githubusercontent.com/Shopify/shopify-app-cli/master/install.sh)"
-
