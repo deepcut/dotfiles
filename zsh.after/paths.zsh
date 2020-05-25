dc69babffc7a43fff04aaff2085aed64929272e4
Feifan Wang <feifanw@gmail.com>
2020-05-25 19:43:29 -0400

    shell history

--- zsh.after/paths.zsh
+++ zsh.after/paths.zsh
@@ -4,3 +4,5 @@ export ASDF_DIR=$(brew --prefix asdf)
 
 [ -f "/Users/fei/.shopify-app-cli/shopify.sh" ] && source "/Users/fei/.shopify-app-cli/shopify.sh"
 
+export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_file_bytes 1024000"
+
