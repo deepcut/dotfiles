82a388cdf6db3d78ef1187dba2a76e13c9b47418
Fei <feifanw@gmail.com>
2020-06-09 01:34:34 -0400

    Remove unnecessary asdf config

--- zsh.after/paths.zsh
+++ zsh.after/paths.zsh
@@ -1,6 +1,5 @@
 export ASDF_DIR=$(brew --prefix asdf)
 . $(brew --prefix asdf)/asdf.sh
-. $(brew --prefix asdf)/etc/bash_completion.d/asdf.bash
 
 [ -f "/Users/fei/.shopify-app-cli/shopify.sh" ] && source "/Users/fei/.shopify-app-cli/shopify.sh"
 
