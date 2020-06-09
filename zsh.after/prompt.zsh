a2fbf0e8cff780ce6c968ee5c996d0f3658e8b62
Fei <feifanw@gmail.com>
2020-06-09 00:10:20 -0400

    Set Starship as default prompt

--- zsh.after/prompt.zsh
+++ zsh.after/prompt.zsh
@@ -1,3 +1,9 @@
 # Set Spaceship ZSH as a prompt
-autoload -U promptinit; promptinit
-prompt spaceship
+# autoload -U promptinit; promptinit
+# prompt spaceship
+# SPACESHIP_PROMPT_ORDER=(user host dir git node exec_time line_sep jobs exit_code char)
+
+# Set Starship as default prompt, faster alternative to Spaceship
+eval "$(starship init zsh)"
+
+
