62456a6b863fd9bd26e01eb10fc03dfd762e7f2c
Feifan Wang <hi@fei.is>
2021-03-08 09:37:39 -0500

    Update bat config

--- zsh.before/setup.zsh
+++ zsh.before/setup.zsh
@@ -3,3 +3,6 @@ BASE16_SHELL="$HOME/.config/base16-shell/"
 [ -n "$PS1" ] && \
     [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
         eval "$("$BASE16_SHELL/profile_helper.sh")"
+
+# Use `bat` to highlight man page
+export MANPAGER="sh -c 'col -bx | bat -l man -p'"
